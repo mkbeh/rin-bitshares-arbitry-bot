3f203395deaa05973556bc253189ba0a750b82f2
noragami <mkbehforever@gmail.com>
2018-12-18 19:54:24 +0300

    add requirements

--- libs/algorithms/bitsharesarbitrage.py
+++ libs/algorithms/bitsharesarbitrage.py
@@ -1,1 +1,27 @@
 # -*- coding: utf-8 -*-
+from pprint import pprint
+
+from bitshares import BitShares
+
+from libs.assetchainsmaker.chainscreator import ChainsCreator
+from libs import utils
+
+
+class BitsharesArbitrage:
+    def foo(self):
+        testnet = BitShares(
+            "wss://bitshares.openledger.info/ws",
+        )
+
+        testnet.wallet.unlock("P5K9YsxTj3YrPPM4ZKBrxcA7sgL1DkSRXw6DmDPJNo3yx")
+        testnet.transfer("makar0ff", 1, "BTS", account="mkbehforever007")
+
+    # def __init__(self, loop):
+    #     self._file_with_chains = ChainsCreator(loop).start_creating_chains()
+    #     self.ioloop = loop
+    #
+    # def _get_chains_from_file(self):
+    #     try:
+    #         return utils.clear_each_str_in_seq(utils.read_file(self._file_with_chains), '\n', ' ')
+    #     except Exception as err:
+    #         raise Exception(err)
--- rin.py
+++ rin.py
@@ -3,6 +3,7 @@ import logging
 import asyncio
 
 from libs.assetchainsmaker.chainscreator import ChainsCreator
+from libs.algorithms.bitsharesarbitrage import BitsharesArbitrage
 
 
 logger = logging.getLogger('Rin')
@@ -11,12 +12,13 @@ logger = logging.getLogger('Rin')
 class Rin:
     @staticmethod
     def start_arbitrage():
-        ioloop = asyncio.get_event_loop()
-
-        try:
-            file_with_chains = ChainsCreator(ioloop).start_creating_chains()
-        finally:
-            ioloop.close()
+        BitsharesArbitrage().foo()
+        # ioloop = asyncio.get_event_loop()
+        #
+        # try:
+        #     file_with_chains = ChainsCreator(ioloop).start_creating_chains()
+        # finally:
+        #     ioloop.close()
 
 
 if __name__ == '__main__':

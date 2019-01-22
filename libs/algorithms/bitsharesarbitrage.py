cab480f4cc23326fb7879ef1d68844c29c358d62
mkbeh <mkbehforever@gmail.com>
2019-01-22 15:03:43 +0300

    Made in BitsharesArbitrage module: - added forever loop for dynamic getting all required data and testing algorithm. - added time limit loop for algorithm testing.

--- libs/algorithms/bitsharesarbitrage.py
+++ libs/algorithms/bitsharesarbitrage.py
@@ -32,36 +32,32 @@ class BitsharesArbitrage(BaseRin):
         return pair.split(':')
 
     @staticmethod
-    async def run_chain_data_thorough_algo(arr):
+    async def run_chain_data_thorough_algo(arr, fees):
         price0, quote0, base0, price1, quote1, base1, price2, quote2, base2 = arr
 
-        with localcontext() as ctx:
-            ctx.prec = 12
-            ctx.rounding = ROUND_HALF_UP
+        if quote0 > base1:
+            quote0 = base1
+            base0 = Decimal(quote0) * Decimal(price0)
 
-            if quote0 > base1:
-                quote0 = base1
-                base0 = Decimal(quote0) * Decimal(price0)
+        elif quote0 < base1:
+            base1 = quote0
+            quote1 = Decimal(base1) / Decimal(price1)
 
-            elif quote0 < base1:
-                base1 = quote0
-                quote1 = Decimal(base1) / Decimal(price1)
+        if quote1 > base2:
+            quote1 = base2
+            base1 = Decimal(quote1) * Decimal(price1)
+            quote0 = base1
+            base0 = Decimal(quote0) * Decimal(price0)
 
-            if quote1 > base2:
-                quote1 = base2
-                base1 = Decimal(quote1) * Decimal(price1)
-                quote0 = base1
-                base0 = Decimal(quote0) * Decimal(price0)
+        elif quote1 < base2:
+            base2 = quote1
+            quote2 = Decimal(base2) / Decimal(price2)
 
-            elif quote1 < base2:
-                base2 = quote1
-                quote2 = Decimal(base2) / Decimal(price2)
+        if base0 < quote2:
+            print(base0, quote2)
 
-            if base0 < quote2:
-                print(base0, quote2)
-
-                profit = Decimal(quote2) - Decimal(base0)
-                print(f'Profit = {profit}! Set orders!\n')
+            profit = Decimal(quote2) - Decimal(base0)
+            print(f'Profit = {profit}! Set orders!\n')
 
     @staticmethod
     async def _get_orders_data_for_chain(chain, gram_markets):
@@ -75,48 +71,38 @@ class BitsharesArbitrage(BaseRin):
             return order_data
 
         pairs_orders_data = await asyncio.gather(
-            # здесь по идее можно вместо генератора списка использовать просто генераторное выражение.
-            *[get_order_data_for_pair(pair, market) for pair, market in zip(chain, gram_markets)]
+            *(get_order_data_for_pair(pair, market) for pair, market in zip(chain, gram_markets))
         )
 
         [arr.extend(pairs_orders_data[i]) for i in range(len(chain))]
 
         return arr
 
-    async def _algorithm_testing(self, chain):
-        # здесь по идее можно вместо генератора списка использовать просто генераторное выражение.
+    async def _algorithm_testing(self, chain, assets_fees):
         markets = [Market() for _ in range(len(chain))]
         [await market.alternative_connect() for market in markets]
-        arr = await self._get_orders_data_for_chain(chain, markets)
-        await self.run_chain_data_thorough_algo(arr)
+
+        time_start = dt.now()
+        time_delta = 0
+
+        while time_delta < DATA_UPDATE_TIME:
+            arr = await self._get_orders_data_for_chain(chain, markets)
+            await self.run_chain_data_thorough_algo(arr, assets_fees)
+
+            time_end = dt.now()
+            time_delta = (time_end - time_start).seconds / 3600
+
+            break
 
         [await market.close() for market in markets]
 
     def start_arbitrage(self):
-        # TODO бесконечный цикл со временем требуется перенести в метод algorithm testing.
-        # TODO передавать доп. параметром % по парам.
-        # TODO заменить в каждом модуле возвращение файла на возвращение значений.
-
         while True:
-            # time_start = dt.now()
-            # time_delta = 0
-
-            """
-            Здесь выполняется следующее:
-            1. Получение цепочек с комиссиями (+)
-            2. Дефолтная комиссия за ордер 
-            3. Объемы лимитов (+)
-            """
             chains = ChainsWithGatewayPairFees(self._ioloop).get_chains_with_fees()
             self._vol_limits = VolLimits(self._ioloop).get_volume_limits()
             self._bts_default_fee = DefaultBTSFee(self._ioloop).get_converted_default_bts_fee()
 
-            break
-            # chains = self._get_chains(self._file_with_chains)
+            tasks = [self._ioloop.create_task(self._algorithm_testing(chain.chain, chain.fees)) for chain in chains[:1]]
+            self._ioloop.run_until_complete(asyncio.gather(*tasks))
 
-            # while time_delta < DATA_UPDATE_TIME:
-            #     tasks = [self._ioloop.create_task(self._algorithm_testing(chain)) for chain in chains]
-            #     self._ioloop.run_until_complete(asyncio.gather(*tasks))
-            #
-            #     time_end = dt.now()
-            #     time_delta = (time_end - time_start).seconds / 3600
+            break
